8edae1d4751038f7c2c84d1410a2d7d4a07c1a37
Rohan Jain <dcrodjer@gmail.com>
2011-03-04 23:15:01 +0530

    version change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 from setuptools import find_packages
 
 setup(name='django-valuate',
-      version='1.0-beta-2',
+      version='1.0-beta-2.2',
       description='Easy plugging of user evaluation system to any django object.',
       long_description=open('README').read(),
       author='Rohan Jain',
