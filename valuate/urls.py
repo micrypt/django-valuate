68298dbb186d28721cd9c93a2fa5735651595bc0
Seyi Ogunyemi <micrypt@gmail.com>
2011-09-22 01:49:50 +0100

    Removed extraneous model import

--- valuate/urls.py
+++ valuate/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, url
 import django.views.generic as gen_views
-#from main.models import *
 
 urlpatterns = patterns('',
     url(r'^submit/$', 'valuate.views.submit', name = 'valuate-submit'),    
